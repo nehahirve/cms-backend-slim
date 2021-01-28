92cc6de76f02fc5e95b0bc6f49ee4783c1fc1220
nehahirve <neha.hirve@hyperisland.se>
2021-01-28 23:05:12 +0100

    test

--- public/api/index.php
+++ public/api/index.php
@@ -5,7 +5,11 @@ use App\Api;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+echo getenv('MYSQL_SSL_CA');
+echo getenv('MYSQL_SSL_KEY');
 echo getenv('MYSQL_SSL_CERT');
+
+
 
 (new Api())->run();
 
